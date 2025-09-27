5ba557f6c90782c2c3f0975d323786746452553e
Pichash69 <AJAY9523908872@GMAIL.COM>
2025-09-27 13:48:11 +0530

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+function generate() {
+  const min = parseInt(document.getElementById('min').value);
+  const max = parseInt(document.getElementById('max').value);
 
+  if (isNaN(min) || isNaN(max) || min > max) {
+    document.getElementById('number').innerText = "Invalid Range!";
+    return;
+  }
+
+  const randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
+  document.getElementById('number').innerText = randomNum;
+}
